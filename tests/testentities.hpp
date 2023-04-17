b6e49ef971e3776c655459699e22656c7a4ecba2
Arijan Amigh <amigharijan@gmail.com>
2023-04-17 02:42:01 +0200

    test: update test entity

--- tests/testentities.hpp
+++ tests/testentities.hpp
@@ -10,17 +10,14 @@ struct Person : scopefn::ScopeFunctions<Person>
 {
     std::string name;
     std::string location;
-    std::vector<Person> children;
     unsigned age;
     void moveTo(std::string newLocation) { location = newLocation; }
 
     bool operator==(Person& other)
     {
-        if(name != other.name || location != other.location || age != other.age || children.size() != other.children.size())
+        if(name != other.name || location != other.location || age != other.age)
             return false;
-        
         return true;
-        // return std::equal(children.begin(), children.end(), other.children.begin());
     }
 };
 
